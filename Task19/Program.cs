52ebff71211990574fb14ddf514142ecabbd8a9d
Alexey <chabrchikov@mail.ru>
2023-06-27 19:57:06 +0300

    add info about homework

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,3 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-System.Console.WriteLine(ppasorjasro);
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
